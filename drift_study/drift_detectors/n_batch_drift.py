78e5755be9529713e5cb4e97c3225aaeacd4b2cb
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2022-06-02 10:29:59 +0200

    Correct n_batch drift

--- drift_study/drift_detectors/n_batch_drift.py
+++ drift_study/drift_detectors/n_batch_drift.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 class NBatchDrift:
@@ -14,15 +15,23 @@ class NBatchDrift:
         self.mem = []
 
     def fit(self, x, t, y, y_scores):
-        self.drift_detector.fit(self, x, t, y, y_scores)
+        self.drift_detector.fit(x, t, y, y_scores)
         self.mem = []
+        self.counter = 0
 
     def update(self, x, t, y, y_scores):
         self.mem.append((x, t, y, y_scores))
         self.counter += 1
         if self.counter >= self.batch_size:
-            x, t, y, y_scores = self.drift_detector.update(zip(*self.mem))
+            x, t, y, y_scores = zip(*self.mem)
+            x, t, y, y_scores = (
+                pd.DataFrame(x),
+                pd.Series(t),
+                np.array(y),
+                np.array(y_scores),
+            )
             self.mem = []
+            self.counter = 0
             return self.drift_detector.update(x, t, y, y_scores)
 
         return False, False, np.nan, np.nan
