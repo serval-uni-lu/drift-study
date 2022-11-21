55996d8dbed06b878eec011f71a17b5e20c37d2d
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2022-11-21 11:41:24 +0100

    model architecture

--- drift_study/model_arch/torch_models.py
+++ drift_study/model_arch/torch_models.py
@@ -68,4 +68,71 @@ class ElectricityModel(BaseModelTorch):
         )
 
 
-models = [("mlp_electricity", ElectricityModel)]
+class LcldNet(nn.Module):
+    def __init__(self, task: str, input_dim=47):
+        super(LcldNet, self).__init__()
+        self.task = task
+
+        self.layers = nn.ModuleList()
+
+        # Input Layer (= first hidden layer)
+        self.input_layer = nn.Linear(input_dim, 32)
+
+        # Hidden Layers (number specified by n_layers)
+        self.layers.extend(
+            [
+                nn.Dropout(0.5),
+                nn.Linear(32, 32),
+                nn.Dropout(0.5),
+            ]
+        )
+
+        # Output Layer
+        self.output_layer = nn.Linear(32, 1)
+
+    def forward(self, x):
+        x = F.relu(self.input_layer(x))
+
+        # Use ReLU as activation for all linear hidden layers
+        for layer in self.layers:
+            if isinstance(layer, nn.Linear):
+                x = F.relu(layer(x))
+            else:
+                x = layer(x)
+
+        x = self.output_layer(x)
+
+        if self.task == "classification":
+            x = F.softmax(x, dim=1)
+
+        return x
+
+
+class LcldModel(BaseModelTorch):
+    def __init__(
+        self,
+        batch_size=64,
+        epochs=10,
+        early_stopping_rounds=2,
+        learning_rate=0.001,
+        class_weight="balanced",
+        **kwargs,
+    ):
+        name = "mlp_lcld"
+        objective = "binary"
+        super().__init__(
+            name,
+            objective,
+            batch_size,
+            epochs,
+            early_stopping_rounds,
+            learning_rate,
+            class_weight=class_weight,
+            **kwargs,
+        )
+        self.model = LcldNet(
+            self.objective,
+        )
+
+
+models = [("mlp_electricity", ElectricityModel), ("mlp_lcld", LcldModel)]
