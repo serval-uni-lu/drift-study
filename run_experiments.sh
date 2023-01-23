61644ff061bc958a3d9fa37a44d982be309dcb77
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-01-23 11:44:14 +0100

    Remove unused

--- run_experiments.sh
+++
@@ -1,4 +0,0 @@
-python -m src.run_simulator -c config/electricity.yaml -c config/electricity_rf.yaml -c config/runs_common.yaml -c config/runs_rf.yaml
-python -m src.run_simulator -c config/electricity.yaml -c config/electricity_nn.yaml -c config/runs_common.yaml
-python -m src.run_simulator -c config/lcld.yaml -c config/lcld_rf.yaml -c config/runs_common.yaml -c config/runs_rf.yaml
-python -m src.run_simulator -c config/lcld.yaml -c config/lcld_nn.yaml -c config/runs_common.yaml
