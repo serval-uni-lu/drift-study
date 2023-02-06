b26ea4bac64dfca69515189f83c0a647acbce19d
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-02-06 16:35:21 +0100

    Move date index in utils

--- drift_study/run_simulator.py
+++ drift_study/run_simulator.py
@@ -6,7 +6,6 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 
 import configutils
 import numpy as np
-import pandas as pd
 from configutils.utils import merge_parameters
 from mlc.metrics.metric_factory import create_metric
 from mlc.metrics.metrics import PredClassificationMetric
@@ -30,18 +29,6 @@ logger = logging.getLogger(__name__)
 warnings.filterwarnings(
     "ignore", category=ExperimentalWarning, module="optuna.*"
 )
-
-
-def find_index_in_past(
-    current_index: int, series: pd.Series, delta: Dict[str, int], past: bool
-) -> int:
-    current_date = series[current_index]
-    if past:
-        past_date = current_date - pd.DateOffset(**delta)
-        return np.argwhere(series < past_date)[0][-1] + 1
-    else:
-        future_date = current_date + pd.DateOffset(**delta)
-        return np.argwhere(series > future_date)[0][0] - 1
 
 
 def run(
