fab6392bb48be73c1313fbfeefa4c8723bfe5352
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-02-27 09:31:09 +0100

    New optimize script

--- scripts/optimize_new.sh
+++ scripts/optimize_new.sh
@@ -3,13 +3,17 @@ for dataset in lcld
 do
     for delay in delays_all, delay_none
     do
-        echo "python
-              -m drift_study.optimize_simulator
-              -c config/${dataset}_opt.yaml
-              -c config/${delay}.yaml
-              -c config/${dataset}_rf.yaml
-              -c config/rf_optimize.yaml
-              -p sub_dir_path=opt_delays_all
-              -p models_dir=./models"
+        for n_input in 100000 300000
+        do
+            echo "python
+                  -m drift_study.optimize_simulator
+                  -c config/${dataset}_opt.yaml
+                  -c config/${delay}.yaml
+                  -c config/${dataset}_rf.yaml
+                  -c config/rf_optimize.yaml
+                  -p common_runs_params.end_train_idx=${n_input}
+                  -p sub_dir_path=opt_${n_input}_${delay}
+                  -p models_dir=./models"
+        done
     done
 done
