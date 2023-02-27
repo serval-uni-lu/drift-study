293ff9a69024ec7e9aa25bce1920259d0f4765af
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-02-27 09:35:56 +0100

    Script

--- scripts/optimize_new.sh
+++ scripts/optimize_new.sh
@@ -5,13 +5,13 @@ do
     do
         for n_input in 100000 300000
         do
-            echo "python
-                  -m drift_study.optimize_simulator
-                  -c config/${dataset}_opt.yaml
-                  -c config/${delay}.yaml
-                  -c config/${dataset}_rf.yaml
-                  -c config/rf_optimize.yaml
-                  -p common_runs_params.end_train_idx=${n_input}
+            echo "python \
+                  -m drift_study.optimize_simulator \
+                  -c config/${dataset}_opt.yaml \
+                  -c config/${delay}.yaml \
+                  -c config/${dataset}_rf.yaml \
+                  -c config/rf_optimize.yaml \
+                  -p common_runs_params.end_train_idx=${n_input} \
                   -p sub_dir_path=opt_${n_input}_${delay}"
         done
     done
