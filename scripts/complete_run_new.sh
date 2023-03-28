3e642f236d50ea32f5bf3200dce36cfc8c36a4da
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-03-28 19:23:07 +0200

    Saving run with evaluation

--- scripts/complete_run_new.sh
+++ scripts/complete_run_new.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 for dataset in lcld
 do
-    for delay in F delays_all
+    for delay in delays_all
     do
-        for n_input in 100000 300000
-        do
-            eval "python \
-                  -m drift_study.run_simulator \
-                  -c config/logging.yaml \
-                  -c config/${dataset}_full.yaml \
-                  -c config/${delay}.yaml \
-                  -c config/${dataset}_rf_opt.yaml \
-                  -c config/${dataset}_runs_opt.yaml \
-                  -p common_runs_params.end_train_idx=${n_input} \
-                  -p sub_dir_path=opt_${n_input}_${delay}"
-        done
+        eval "python \
+              -m drift_study.run_simulator \
+              -c config/logging.yaml \
+              -c config/${dataset}_full.yaml \
+              -c config/${delay}.yaml \
+              -c config/${dataset}_rf_opt.yaml \
+              -c config/${dataset}_runs_opt.yaml \
+              -p sub_dir_path=full_${delay}"
     done
 done
