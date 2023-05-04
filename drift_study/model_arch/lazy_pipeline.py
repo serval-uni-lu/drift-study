8ff5830591219de08b8b3b4eb2cd0ed1f314c7f9
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-05-04 13:32:32 +0200

    Correct lazy pipeline

--- drift_study/model_arch/lazy_pipeline.py
+++ drift_study/model_arch/lazy_pipeline.py
@@ -89,6 +89,6 @@ class LazyPipeline:
         self.save_pred(x)
         return self.lazy_predict(start_idx, end_idx)
 
-    def fit(self, x, y, x_val, y_val):
+    def fit(self, x, y, x_val=None, y_val=None):
         self._pipeline_load()
         self.pipeline.fit(x, y, x_val, y_val)
