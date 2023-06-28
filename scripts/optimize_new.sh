3b9728c20e91f748cdb7c1a5524efdec44851fef
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-06-28 13:37:01 +0200

    Optimize script

--- scripts/optimize_new.sh
+++ scripts/optimize_new.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 for dataset in lcld
 do
-    for delay in delays_all delays_none
+    for delay in delays_all
     do
-        for n_input in 100000 300000
+        for n_input in 300000
         do
             eval "python \
                   -m drift_study.optimize_simulator \
                   -c config/logging.yaml \
                   -c config/${dataset}_opt.yaml \
                   -c config/${delay}.yaml \
-                  -c config/${dataset}_rf.yaml \
-                  -c config/rf_optimize.yaml \
+                  -c config/${dataset}_rf_opt.yaml \
+                  -c config/${dataset}_rf_complete.yaml \
                   -p common_runs_params.end_train_idx=${n_input} \
                   -p sub_dir_path=opt_${n_input}_${delay}"
         done
