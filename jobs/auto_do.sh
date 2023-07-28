3f6997adb115496c87946534df87d37d903dacb1
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-07-28 13:26:12 +0200

    Job

--- jobs/auto_do.sh
+++ jobs/auto_do.sh
@@ -1,1 +1,2 @@
 python -m drift_study.run.pretrain_models -c config/logging.yaml -c ./config/lcld_auto.yaml -p pretrain_periodic=true -p use_auto_model_tuning=true
+python -m drift_study.run.optimize_detector -c config/logging.yaml -c ./config/lcld_auto.yaml  -p schedule_data_path=./data/drift/lcld_201317_ds_time/rf_scaled/detector_opt/
