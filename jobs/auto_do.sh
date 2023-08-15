fd2c344f02e064bc0a51726177b86c0c516019a5
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-08-15 16:55:50 +0200

    Auto do script

--- jobs/auto_do.sh
+++ jobs/auto_do.sh
@@ -1,2 +1,6 @@
-python -m drift_study.run.pretrain_models -c config/logging.yaml -c ./config/lcld_auto.yaml -p pretrain_periodic=true -p use_auto_model_tuning=true
-python -m drift_study.run.optimize_detector -c config/logging.yaml -c ./config/lcld_auto.yaml  -p schedule_data_path=./data/drift/lcld_201317_ds_time/rf_scaled/detector_opt/
+AUTO_CONFIG=./config/lcld_rln.yaml; python -m drift_study.run.optimize_model -c config/logging.yaml -c ${AUTO_CONFIG}
+
+
+python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p use_auto_model_tuning=true
+python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p use_auto_model_tuning=true
+python -m drift_study.run.optimize_detector -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/lcld_201317_ds_time/rf_scaled/detector_opt/
