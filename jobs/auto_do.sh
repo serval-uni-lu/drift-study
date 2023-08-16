b0a20205330e22e7dbf0b2094f9c48ab9e31bd08
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-08-16 16:51:23 +0200

    Table 2 conf

--- jobs/auto_do.sh
+++ jobs/auto_do.sh
@@ -1,14 +1,25 @@
 DATASET_NAME=electricity
 MODEL_NAME=rf_scaled
 AUTO_CONFIG=./config/auto/${DATASET_NAME}_${MODEL_NAME}.yaml
+# Prepare
+
 # python -m drift_study.run.optimize_model -c config/logging.yaml -c ${AUTO_CONFIG}
+
+# TABLE 1
+
 # python -m drift_study.run.no_retrain_baseline -c config/logging.yaml -c ${AUTO_CONFIG} -p use_auto_model_tuning=true -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/no_retrain/
 # python -m drift_study.visualization.line_graph_auto -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/ -p plot_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/baseline.html
 # python -m drift_study.run.manual_baseline -c config/logging.yaml -c ${AUTO_CONFIG} -p use_auto_model_tuning=true -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/manual/
-python -m drift_study.visualization.line_graph_auto -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/ -p plot_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/baseline_manual.html
+# python -m drift_study.visualization.line_graph_auto -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/ -p plot_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/baseline_manual.html
 
-python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p use_auto_model_tuning=true
-python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p model.optimize=true
+# # TABLE 2
+# python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p use_auto_model_tuning=true -p train_window_sizes=17760
+# python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p model.optimize=true -p train_window_sizes=17760
+
+python -m drift_study.run.periodic_baseline -c config/logging.yaml -c ${AUTO_CONFIG} -p use_auto_model_tuning=true -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/periodic/
+python -m drift_study.run.periodic_baseline -c config/logging.yaml -c ${AUTO_CONFIG} -p model.optimize=true -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/opt_periodic/
+
+
 
 # python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p use_auto_model_tuning=true
 # python -m drift_study.run.optimize_detector -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/lcld_201317_ds_time/rf_scaled/detector_opt/
