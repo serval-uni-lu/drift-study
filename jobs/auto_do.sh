ca43ed97067c71002c5b59c2b3391cda5ddc0a8e
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-08-17 12:56:21 +0200

    scripts

--- jobs/auto_do.sh
+++ jobs/auto_do.sh
@@ -1,6 +1,16 @@
 DATASET_NAME=electricity
 MODEL_NAME=rf_scaled
 AUTO_CONFIG=./config/auto/${DATASET_NAME}_${MODEL_NAME}.yaml
+MODEL_TYPE=rf
+
+RETRAIN_OPT=true
+extra_params=""
+if [ "$RETRAIN_OPT" = true ]; then
+    extra_params="-p model.optimize=true"
+else
+    extra_params="-p use_auto_model_tuning=true"
+fi
+
 # Prepare
 
 # python -m drift_study.run.optimize_model -c config/logging.yaml -c ${AUTO_CONFIG}
@@ -14,12 +24,18 @@ AUTO_CONFIG=./config/auto/${DATASET_NAME}_${MODEL_NAME}.yaml
 
 # # TABLE 2
 # python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p use_auto_model_tuning=true -p train_window_sizes=17760
-# python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p model.optimize=true -p train_window_sizes=17760
 
-python -m drift_study.run.periodic_baseline -c config/logging.yaml -c ${AUTO_CONFIG} -p use_auto_model_tuning=true -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/periodic/
-python -m drift_study.run.periodic_baseline -c config/logging.yaml -c ${AUTO_CONFIG} -p model.optimize=true -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/opt_periodic/
+# python -m drift_study.run.periodic_baseline -c config/logging.yaml -c ${AUTO_CONFIG} -p use_auto_model_tuning=true -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/periodic/ -c ./config/auto/delays_none.yaml
+# python -m drift_study.run.periodic_baseline -c config/logging.yaml -c ${AUTO_CONFIG} -p model.optimize=true -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/opt_periodic/ -c ./config/auto/delays_none.yaml
+
+# TABLE 3
+# python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true ${extra_params}
+# python -m drift_study.run.train_window_size -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/train_window_size ${extra_params}
+# python -m drift_study.run.train_window_size -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/train_window_size_no_delays ${extra_params} -c ./config/auto/delays_none.yaml
+# python -m drift_study.visualization.auto_table -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/train_window_size -p out_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/train_window_size.csv
+# python -m drift_study.visualization.auto_table -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/train_window_size_no_delays -p out_path=./data/drift/${DATASET_NAME}/${MODEL_NAME}/train_window_size_no_delays.csv
 
 
 
-# python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_periodic=true -p use_auto_model_tuning=true
-# python -m drift_study.run.optimize_detector -c config/logging.yaml -c ${AUTO_CONFIG} -p schedule_data_path=./data/drift/lcld_201317_ds_time/rf_scaled/detector_opt/
+# python -m drift_study.run.pretrain_models -c config/logging.yaml -c ${AUTO_CONFIG} -p pretrain_schedules_opt=true ${extra_params}
+python -m drift_study.run.optimize_detector -c config/logging.yaml -c ${AUTO_CONFIG} -c config/auto/schedules_${MODEL_TYPE}.yaml -p schedule_data_path=./data/drift//${DATASET_NAME}/${MODEL_NAME}/detector_opt/ ${extra_params}
---
+++ launch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#SBATCH -o %x_%j.out
+#SBATCH -N 1
+#SBATCH -c 128
+#--mem=32GB
+#SBATCH -p batch
+#SBATCH --time=0-1:00:00
+#SBATCH --qos=normal
+#SBATCH --mail-type=all
+#SBATCH --mail-user=thibault.simonetto@uni.lu
+
+echo "Hello from the batch queue on node ${SLURM_NODELIST} for neural architecture generation"
+# export LOGLEVEL="ERROR"
+# export PYTHONWARNINGS="ignore"
+# export MODELS_DIR="/scratch/users/tsimonetto/drift-study/models2"
+# export MODELS_DIR="/scratch/users/tsimonetto/drift-study/models"
+conda activate drift2
+
+"$@"
+
