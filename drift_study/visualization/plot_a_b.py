4075d47a3d24050dddaa121a394228ca6668e777
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2023-08-21 08:57:53 +0200

    Visualization

---
+++ drift_study/visualization/plot_a_b.py
@@ -0,0 +1,57 @@
+import pandas as pd
+from drift_study.reports.graphics import scatterplot
+
+
+def filter_extreme(df, max_n_train, max_pareto_rank=1):
+    filter_baseline = df["schedule_type"] == "Baseline"
+    filter_others = (df["n_train"] > 1) & (df["n_train"] < max_n_train) & (df["pareto_rank"] <= max_pareto_rank)
+    df = df[filter_baseline | filter_others]
+    return df
+
+def run():
+    path_delays = "./detector.html.csv"
+    path_no_delays ="./detector_no_delays.html.csv"
+    
+    df_delays = pd.read_csv(path_delays)
+    df_no_delays = pd.read_csv(path_no_delays)
+    max_n_train = 54
+    
+    df_delays = filter_extreme(df_delays, max_n_train)
+    df_delays["Type"] = df_delays["schedule_type"]
+    df_no_delays = filter_extreme(df_no_delays, max_n_train)
+    df_no_delays["Type"] = df_no_delays["schedule_type"]
+    
+    max_y = max(df_delays["ml_metric"].max(), df_no_delays["ml_metric"].max())
+    min_y = min(df_delays["ml_metric"].min(), df_no_delays["ml_metric"].min())
+    
+    delta = max_y - min_y
+    
+    max_y += delta * 0.1
+    min_y -= delta * 0.1
+        
+    type_to_marker = {
+        "Baseline": "x",
+        "Data": "s",
+        "Error": "^",
+        "Predictive": "o",
+    }
+
+    for name, df in zip(["delays", "no_delays"], [df_delays, df_no_delays]):
+        n_markers = df["Type"].unique()
+        markers = [type_to_marker[e] for e in n_markers]
+        scatterplot(
+                df,
+                f"{name}.pdf",
+                x="n_train",
+                y="ml_metric",
+                y_label="MCC",
+                hue="Type",
+                x_label="\\# Train",
+                fig_size=(6, 4),
+                legend_pos="best",
+                markers=markers,
+                ylim=(min_y, max_y),
+            )
+    
+if __name__ == "__main__":
+    run()
---
+++ drift_study/visualization/plot_many_lcld.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from drift_study.reports.graphics import scatterplot, lineplot
+
+
+def filter_extreme(df, max_pareto_rank=1):
+    filter_others = df["pareto_rank"] <= max_pareto_rank
+    df = df[filter_others]
+    return df
+
+
+def run():
+    path_delays = "./lcld/rq2.html.csv"
+    # path_no_delays = "./detector_no_delays.html.csv"
+    path_half_delays = "./lcld/rq4.1.html.csv"
+    path_twice_delays = "./lcld/rq4.2.html.csv"
+
+    df_delays = pd.read_csv(path_delays)
+    # df_no_delays = pd.read_csv(path_no_delays)
+    df_half_delays = pd.read_csv(path_half_delays)
+    df_twice_delays = pd.read_csv(path_twice_delays)
+
+    df_delays = filter_extreme(df_delays)
+    # df_no_delays = filter_extreme(df_no_delays)
+    df_half_delays = filter_extreme(df_half_delays)
+    df_twice_delays = filter_extreme(df_twice_delays)
+    
+    
+    df_half_delays["delay"] = "$\\delta_d / 2$"
+    df_delays["delay"] = "$\\delta_d$"
+    df_twice_delays["delay"] = "$2\\delta_d$"
+
+
+    df_all = pd.concat([df_delays, df_half_delays, df_twice_delays])
+
+    lineplot(
+        df_all,
+        "pareto.pdf",
+        x="n_train",
+        y="ml_metric",
+        y_label="MCC",
+        hue="delay",
+        x_label="\\# Train",
+        fig_size=(6, 4),
+        legend_pos="best",
+    )
+
+
+if __name__ == "__main__":
+    run()
