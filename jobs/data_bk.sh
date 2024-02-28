7482b6b75e551d74c8767afd4b07de984e78174f
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2024-02-28 12:41:49 +0100

    Use tar instead of zip

--- jobs/data_bk.sh
+++ jobs/data_bk.sh
@@ -1,3 +1,5 @@
-now=$(date +%Y-%m-%d--%H%-M-%S)
+now=$(date +%Y-%m-%d--%H-%M-%S)
 echo $now
-zip -r drift_study_data_bk_${now}.zip ./data
+# zip -r drift_study_data_bk_${now}.zip ./data
+time tar -cvf drift_study_data_bk_${now}.tar ./data
+# time tar -zcvf drift_study_data_bk_${now}.tar.gz ./data
