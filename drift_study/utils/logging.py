7dcb3f78d0acd30a95e37c4d088f14df5e005510
Thibault Simonetto <thibault.simonetto.001@student.uni.lu>
2024-02-28 08:21:30 +0100

    Logging configuration

--- drift_study/utils/logging.py
+++ drift_study/utils/logging.py
@@ -1,12 +1,36 @@
 import logging.config
 import warnings
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from optuna.exceptions import ExperimentalWarning
 
+DEFAULT_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "simple": {
+            "format": "[%(levelname)-.4s] %(asctime)s - %(name)s - %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "simple",
+            "stream": "ext://sys.stdout",
+        }
+    },
+    "loggers": {
+        "__main__": {"level": "DEBUG"},
+        "optuna": {"level": "ERROR"},
+        "matplotlib": {"level": "ERROR"},
+    },
+    "root": {"level": "DEBUG", "handlers": ["console"]},
+}
 
-def configure_logger(config: Dict[str, Any]):
-    logging_config = config.get("logging")
-    if logging_config is not None:
-        logging.config.dictConfig(logging_config)
+
+def configure_logger(config: Optional[Dict[str, Any]] = None):
+    if config is None:
+        config = {}
+    logging_config = config.get("logging", DEFAULT_CONFIG)
+    logging.config.dictConfig(logging_config)
     warnings.filterwarnings("ignore", category=ExperimentalWarning)
